0e67fa8d994562b44e3b83ecfc03e948b8437659
Sandeepkondreddy <Sandeepkondreddy@gmail.com>
2017-10-18 09:03:43 +0530

    CT Operations- SDS Admin screen Added

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,3 +1,4 @@
+var qsParm = new Array();
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
@@ -15,7 +16,7 @@ $(document).ready(function(){
 		showUserRecords();
         $("#loading").hide();
         $(".box5").click(function(){
-            $("#loading").show();debugger;
+            $("#loading").show();
             window.location.href = 'RegDevice.html?user=' + btoa($("#hidusrid").val()) + '';
         });
         $(".box7").click(function(){
@@ -24,21 +25,14 @@ $(document).ready(function(){
         });
 		$(".box6").click(function(){debugger;
             $("#loading").show();
-			$.ajax({
-             type: "GET",
-             //url: "http://202.83.27.199/TestAPI/api/User/GetUserScreens/" + $("#hidusrid").val(),		//Act Link.						
-			 url: "http://182.72.244.25/KPCTSDS/api/Account/GetUserScreens/" + $("#hidusrid").val(),	//Airtel Link.
-             data: '{}',
-             contentType: "application/json",
-             success: function(result) {
-             window.location.href = result + '?user=' + btoa($("#hidusrid").val());
-			}
-            });
-            //window.location.href = 'admin_sds.html?user=' + btoa($("#hidusrid").val()) + '';
+			alert($("#hidusrid").val());
+			if(sdsresult!="")
+            window.location.href = sdsresult+'?user=' + btoa($("#hidusrid").val()) + '';
         });
+		
 });
 
-var qsParm = new Array();
+
 function qs() {
         var query = window.location.search.substring(1);
         var parms = query.split('&');
@@ -58,11 +52,42 @@ function qs() {
             window.location.href = 'Login.html';
             return false;
         }
+		
 }
 
-function getSDS()
-{
-	
+var sdsresult="";
+function getSDS(var userr,var passs)
+{debugger;
+	if(user!=""){
+	$.ajax({
+                    type: "GET",
+					//url: "http://202.83.27.199/TestAPI/api/User/ValidateUser/" + $("#txtusername").val().trim() + "/" + $("#txtpassword").val(),	  	//Act Link.                
+					url: "http://182.72.244.25/KPCTSDS/api/Account/ValidateUser/" + userr + "/" + passs,   //Airtel Link.
+                    data: '{}',
+                    contentType: "application/json",
+                    success: function(data) {
+                        if (data[1] == 'True') {
+                            $("#husrid").val(data[0]);
+                            $.ajax({
+                                type: "GET",
+                                //url: "http://202.83.27.199/TestAPI/api/User/GetUserScreens/" + $("#hidusrid").val(),		//Act Link.						
+								url: "http://182.72.244.25/KPCTSDS/api/Account/GetUserScreens/" + $("#husrid").val(),	//Airtel Link.
+                                data: '{}',
+                                contentType: "application/json",
+                                success: function(result) {
+									if(result=='admin.html')result='admin_sds.html';
+                                    window.location.href = result + '?user=' + btoa($("#husrid").val());
+                                }
+                            });
+                        } else {
+                            alert("Invalid User Name or Password");
+                        }
+                    },
+                    error: function() {
+                        alert("Invalid User Name or Password");
+                    }
+                });
+	}
 }
 
 //  Internal (SQL Lite) DB Section-----Start--- 
@@ -91,12 +116,12 @@ function getSDS()
 				 return;
 			 }
 		 }
-		 
+var user="";var pass="";		 
 		 // Function For Retrive User data from Database
 		var selectRecentUserStatement = " SELECT * FROM UserTbl where Id=(Select Max(Id) from UserTbl)";
 		var userDataset;
 		function showUserRecords() // Function For Retrive data from Database Display records as list
-		{debugger;
+		{
 			 db.transaction(function (tx) {
 				 tx.executeSql(selectRecentUserStatement, [], function (tx, result) {
 					 userDataset = result.rows;
@@ -112,7 +137,9 @@ function getSDS()
 					 for (var i = 0, item = null; i < userDataset.length; i++) {
 						item = userDataset.item(i);
 						//alert('Id:'+item['Id']+ ', IMEI:'+item['IMEI']+', LoginId:'+item['LoginId']+', Password:'+item['Password']+', HomePage:'+item['HomePage']+',  CreatedTime:'+item['CreatedTime']);						 
-						 
+						 user=item['LoginId'];
+						 pass=item['Password'];
+						 getSDS(item['LoginId'],item['Password']);
 					 }
 					 
 				 });
